b10987db885e419d0bb9892fedbd07a84f96a0be
Can Delibas <candelibas96@gmail.com>
2024-03-21 19:37:46 +0200

    add files

---
+++ client/main.js
@@ -0,0 +1,41 @@
+import { DiscordSDK } from "@discord/embedded-app-sdk";
+
+import { Boot } from './scenes/Boot';
+import { Game } from './scenes/Game';
+import { GameOver } from './scenes/GameOver';
+import { MainMenu } from './scenes/MainMenu';
+import { Preloader } from './scenes/Preloader';
+
+// Instantiate the SDK
+const discordSdk = new DiscordSDK(import.meta.env.VITE_DISCORD_CLIENT_ID);
+
+setupDiscordSdk().then(() => {
+  console.log("Discord SDK is ready");
+});
+
+async function setupDiscordSdk() {
+  await discordSdk.ready();
+}
+
+//  Find out more information about the Game Config at:
+//  https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
+const config = {
+    type: Phaser.AUTO,
+    width: 1024,
+    height: 768,
+    parent: 'game-container',
+    backgroundColor: '#028af8',
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    },
+    scene: [
+      Boot,
+      Preloader,
+      MainMenu,
+      Game,
+      GameOver
+    ]
+};
+
+ new Phaser.Game(config);

84046e6bbea0d789002fbf92c9c8c65cb864a71f
Can Delibas <candelibas96@gmail.com>
2024-09-30 18:34:19 +0300

    define proxies for vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,19 +1,29 @@
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  envDir: '../',
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
-        secure: false,
-        ws: true,
+
+export default ({ mode }) => {
+  return defineConfig({
+    envDir: '../',
+    server: {
+      proxy: {
+        '/.proxy/assets': {
+          target: 'http://localhost:5173/assets',
+          changeOrigin: true,
+          ws: true,
+          rewrite: (path) => path.replace(/^\/.proxy\/assets/, ''),
+        },
+        '/.proxy/api': {
+          target: 'http://localhost:3001',
+          changeOrigin: true,
+          secure: false,
+          ws: true,
+          rewrite: (path) => path.replace(/^\/.proxy\/api/, ''),
+        },
+      },
+      hmr: {
+        clientPort: 5173,
       },
     },
-    hmr: {
-      clientPort: 5173, //443
-    },
-  },
-});
+  });
+};
